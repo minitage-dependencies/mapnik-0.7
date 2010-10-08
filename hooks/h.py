ec60d41e99baf23816e72629976c9678f9c523f9
kiorky <kiorky@cryptelium.net>
2010-10-08 08:38:11 +0000

    up

--- hooks/h.py
+++ hooks/h.py
@@ -1,5 +1,8 @@
 import os
-def h(options, buildout):
-    """Custom pre-make hook for building libjpeg."""
-    os.environ['PYTHON_PREFIX'] = os.path.abspath(options['location'] + '/../site-packages-%s' % buildout['python']['version'])
-
+def p(options,buildout):
+    """Where python looks to get its cflags."""
+    import pdb;pdb.set_trace()  ## Breakpoint ##
+    fp64 = os.path.join(options['location'], 'lib64')
+    fp = os.path.join(options['location'], 'lib') 
+    if os.path.exists(fp64) and not os.path.exists(fp):
+        os.symlink(fp64, fp) 

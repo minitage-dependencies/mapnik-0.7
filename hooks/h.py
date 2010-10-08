a0ef602aa7e9e21a064d8797d9798e026f64549a
kiorky <kiorky@cryptelium.net>
2010-10-08 09:33:39 +0000

    up

--- hooks/h.py
+++ hooks/h.py
@@ -1,7 +1,6 @@
 import os
 def p(options,buildout):
     """Where python looks to get its cflags."""
-    import pdb;pdb.set_trace()  ## Breakpoint ##
     fp64 = os.path.join(options['location'], 'lib64')
     fp = os.path.join(options['location'], 'lib') 
     if os.path.exists(fp64) and not os.path.exists(fp):
